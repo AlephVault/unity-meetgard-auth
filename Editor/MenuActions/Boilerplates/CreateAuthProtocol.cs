125a7b9e2d469e3339878c1f3f871e654de64c97
Luis Masuelli <luisfmasuelli@gmail.com>
2024-03-11 20:22:08 -0500

    Centralizing the menu items under a new prefix

--- Editor/MenuActions/Boilerplates/CreateAuthProtocol.cs
+++ Editor/MenuActions/Boilerplates/CreateAuthProtocol.cs
@@ -273,7 +273,7 @@ WARNING: THIS MIGHT OVERRIDE EXISTING CODE. Always use proper source code manage
                 /// <summary>
                 ///   Opens a dialog to execute the protocol creation boilerplate.
                 /// </summary>
-                [MenuItem("Assets/Create/Meetgard.Auth/Boilerplates/Create Simple Auth Protocol", false, 201)]
+                [MenuItem("Assets/Create/AlephVault/Meetgard.Auth/Boilerplates/Create Simple Auth Protocol", false, 201)]
                 public static void ExecuteBoilerplate()
                 {
                     CreateAuthProtocolWindow window = ScriptableObject.CreateInstance<CreateAuthProtocolWindow>();
